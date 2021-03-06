45645fdd5fb5bf048c041b28c467e6d4cee02512
liuhuan1024 <liuhuan1024@qq.com>
2021-03-06 21:10:33 +0800

    button ok

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,58 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="row">
+      <lh-button @click="fn">按钮</lh-button>
+      <lh-button type="primary">按钮</lh-button>
+      <lh-button type="info">按钮</lh-button>
+      <lh-button type="warning">按钮</lh-button>
+      <lh-button type="danger">按钮</lh-button>
+    </div>
+    <div class="row">
+      <lh-button>按钮</lh-button>
+      <lh-button plain type="primary">按钮</lh-button>
+      <lh-button plain type="info">按钮</lh-button>
+      <lh-button plain type="warning">按钮</lh-button>
+      <lh-button plain type="danger">按钮</lh-button>
+    </div>
+    <div class="row">
+      <lh-button>按钮</lh-button>
+      <lh-button plain round type="primary">按钮</lh-button>
+      <lh-button plain round type="info">按钮</lh-button>
+      <lh-button plain round type="warning">按钮</lh-button>
+      <lh-button icon="lh-icon-aixin" plain round type="danger">按钮</lh-button>
+    </div>
+    <div class="row">
+      <lh-button>按钮</lh-button>
+      <lh-button plain round circle type="primary">按钮</lh-button>
+      <lh-button plain round circle type="info">按钮</lh-button>
+      <lh-button plain round circle type="warning">按钮</lh-button>
+      <lh-button plain round circle type="danger">按钮</lh-button>
+    </div>
+    <div class="row">
+      <lh-button icon="lh-icon-aixin"></lh-button>
+      <lh-button icon="lh-icon-aixin" plain round circle type="primary"></lh-button>
+      <lh-button icon="lh-icon-aixin" plain round circle type="info"></lh-button>
+      <lh-button icon="lh-icon-aixin" plain round circle type="warning"></lh-button>
+      <lh-button icon="lh-icon-aixin" plain round circle type="danger"></lh-button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  methods: {
+    fn () {
+      console.log('123')
+    }
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.row {
+  margin-bottom: 20px;
+  .lh-button {
+    margin-right: 20px;
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import LhButton from './components/button.vue'
+import './assets/iconfont/iconfont.css'
 Vue.config.productionTip = false
 
+Vue.component(LhButton.name, LhButton)
 new Vue({
   render: h => h(App)
 }).$mount('#app')
